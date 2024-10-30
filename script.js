0bc9865853cbf3d7681bfb1c79c34f3f38f0e3d7
sohan1810 <sohanjaviya123@gmail.com>
2024-10-30 18:44:33 +0530

    javascript_started

---
+++ script.js
@@ -0,0 +1,120 @@
+
+// Linking a JavaScript File
+let js = 'amazing';
+// if (js === 'amazing') alert('javascript is fun!');
+86 + 4 + 20 - 22;
+console.log(86 + 4 + 20 - 22);
+
+// Values and Variables
+console.log("Sohan");
+console.log(22);
+
+// let firstName = "Sohan";
+let first_name = "sahil";
+
+// console.log(firstName);
+console.log(first_name);
+
+
+let jonas_matilda = "sj";
+let $function = 51;
+console.log(jonas_matilda);
+console.log($function);
+
+let person = "java";
+let PI = 4.2345;
+console.log(person);
+console.log(PI);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let job1 = "programmer";
+let job2 = "teacher";
+console.log(job1);
+console.log(job2);
+
+// THE 7 PRIMITIVE DATA TYPES
+// 1. Number.Floating point numbers Used for decimals and integers let age= 23;
+// 2. String: Sequence of characters Used for text let firstName = 'Jonas';
+// 3. Boolean: Logical type that can only be true or false Used for taking decisions let fullAge = true;
+// 4. Undefined: Value taken by a variable that is not yet defined('empty value) let children:
+// 5. Null: Also means 'empty value'
+// 6. Symbol(ES2015); Value that is unique and cannot be changed(Not useful for now)
+// 7. Bigint(ES2020): Larger integers than the Number type can hold
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+// undefined value 
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); 
+
+// let, const and var
+
+let age = 30;
+age = 31;
+age = 38;
+console.log(age);
+
+// Const value will not change any time
+const birthYear = 1971;
+// birthYear = 1975;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+console.log(job);
+
+// do not right the variables like this error in javascript
+// lastName = 'Schmedtmann';
+// console.log(lastName);
+
+// Basic Operators
+// Math operators
+const now = 2024;
+const age1 = now - 1971;
+const age2 = now - 2002;
+console.log(age1, age2);
+
+  
+console.log(age1 * 2, age1 / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'sohan';
+const lastName = 'sahil';
+console.log(firstName + ' ' + lastName);
+
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x /= 2; // x = x / 2 = 50
+x++; // x = x + 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(age1 > age2); // >, <, >=, <=
+console.log(age2 >= 18);
+
+// const isFullAge = age2 >= 18;
+
+console.log(now - 1971 > now - 2002);
