6c4c8ba151e913c18a1e66cc8c0a1617cf67ba9a
sohan1810 <sohanjaviya123@gmail.com>
2024-11-05 19:12:08 +0530

    4_day

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ multiple
 lines`);
 
 
-// Taking Decisions: if / else Statements
+// Taking Decisions: if / else Statements 
 const age1 = 19;
 
 if (age1 >= 18) {
@@ -278,13 +278,14 @@ stri = Number(false); //false = 0 and true = 1
 stri = Number([1, 2, 3, 4, 5, 6, 7, 8, 9]); // if we use array or String it will show NaN = not a number
 // console.log(stri, (typeof stri));
 
-let number = parseFloat('34.098');// parseInt('34')  parseFloat('34.098') 
+let number = parseFloat('34.098');// parseInt('34') parseFloat('34.098') The parseInt method parses a value as a string and returns the first integer.
 
 //The toFixed() method rounds the string to a specified number of decimals.
 // console.log(number.toFixed(2), (typeof number)); 
 
 
 // Type coercion
+// Type coercion refers to the automatic conversion of values from one data type to another
 let mystr = Number("698");
 let mynum = 34;
 // let mynum = '34';// String
@@ -313,8 +314,8 @@ if (height) {
 } else {
   console.log('Height is UNDEFINED');
 }
-*/
-// Equality Operators: == vs. ===
+
+// Equality Operators: (== value are same) vs. (=== value are same and data type same)  
 const age = '18';
 if (age === 18) console.log('You just became an adult :D (strict)');
 
@@ -338,6 +339,8 @@ if (favourite !== 23) console.log('Why not 23?');
 
 
 // Logical Operators
+&&,||, ! are Operators
+
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
 
@@ -360,3 +363,104 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log('Someone else should drive...');
 }
 
+
+
+// Coding Challenge #3
+
+
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+
+// BONUS 1 Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log('Both win the trophy!');
+} else {
+  console.log('No one wins the trophy 😭');
+}
+
+// The switch Statement
+const day = 'wednesday';
+
+switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+if (day === 'wednesday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
+}
+
+
+// // Statements and Expressions
+// 3 + 4
+// 1991
+// true && false && !false
+
+// if (23 > 10) {
+//   const str = '23 is bigger';
+// }
+
+// const me = 'Jonas';
+// console.log(`I'm ${2037 - 1991} years old ${me}`);
+*/
