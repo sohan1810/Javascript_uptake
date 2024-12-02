7c336fc269925bae2e94a11899136288beba8106
sohan1810 <sohanjaviya123@gmail.com>
2024-12-02 19:08:17 +0530

    21_day

---
+++ 7 Section PROJECT 2 Modal/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+
+const overlay = document.querySelector(".overlay");
+
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnOpenModel = document.querySelectorAll(".show-modal");
+console.log(btnOpenModel);
+
+for (let i = 0; i < btnOpenModel.length; i++)
+  btnOpenModel[i].addEventListener("click", function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      modal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    }
+  }
+});
