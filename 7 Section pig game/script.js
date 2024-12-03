1d8dea0faf1d3ccda3e7ff80a12477ebdf9bdf38
sohan1810 <sohanjaviya123@gmail.com>
2024-12-03 19:23:25 +0530

    22_day

--- 7 Section pig game/script.js
+++ 7 Section pig game/script.js
@@ -9,3 +9,13 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+
+
+// /high level
+//object orinted
+//multi progrmed
+//single-thread
+//garbage-collected
+//first-classfunction
+//daynamic
+//Non-event Blocking
