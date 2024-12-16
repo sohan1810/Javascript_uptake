89e5b69ea0dd1219ba1ccc272d7a034b3397c54f
sohan1810 <sohanjaviya123@gmail.com>
2024-12-16 18:25:49 +0530

    30_day

--- 9 Section/script.js
+++ 9 Section/script.js
@@ -244,6 +244,20 @@ console.log("---- AND ----");
 console.log(0 && "Jonas");
 console.log(7 && "Jonas");
 
+
+// / The nullish coalescing operator (??) is a JavaScript operator that is used to provide 
+// a default value for a variable that may be null or undefined.
+
+// In JavaScript, the following values are considered falsy:
+//  false, 0, ""(empty string), null, undefined, and NaN.
+
+//only null, undefined it will pass default value.
+
+let num =5;
+const num1= num ?? 11;
+console.log(num1);
+
+
 // Coding Challenge #1
 
 /* 
@@ -251,13 +265,7 @@ We're building a football betting app (soccer for my American friends 😅)!
 
 Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
 
 TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
@@ -304,24 +312,35 @@ const game = {
     team2: 6.5,
   },
 };
-
+// 1. Create one player array for each team (variables 'players1' and 'players2')
 const [players1, players2] = game.players;
 console.log(players1, players2);
 
+// 2. The first player in any player array is the goalkeeper and the others are field players. 
+// For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array 
+// ('fieldPlayers') with all the remaining 10 field players
 const [gk, ...fieldPlayers] = players1;
 console.log(gk, fieldPlayers);
 
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array 
+// ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 const players1Final = [...players1, "sohan", "sahil", "dev"];
 console.log(players1Final);
 
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 const {
   odds: { team1, x: draw, team2 },
 } = game;
 console.log(team1, draw, team2);
 
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) 
+// and prints each of them to the console, along with the number of goals that were scored in total 
+// (number of player names passed in)
 const printGoals = function (...players) {
   console.log(players);
   console.log(`${players.length} goals were scored`);
@@ -337,4 +356,45 @@ team1 > team2 && console.log('Team 2 is more likely to win');
 const menu = [...allPlayers];
 
 for (const item of menu) console.log(item);
+
+// The for-of Loop
+const array1 = ['a', 'b', 'c'];
+
+for (const element of array1) {
+  console.log(element);
+}
+
+//enhanced object literals javascript
+// let name = "sohan";
+// let age = 22;
+// let obj = {
+//     name,age
+// };
+// console.log(obj);
+
+let n = "name";
+var obj = {
+    [n]:"sohan",
+    age : 22
+};
+console.log(obj);
+
+let m = "first";
+var obj = {
+    [m + " name"]:"sohan",
+    age : 22
+};
+console.log(obj);
+
+
+let s = "first";
+var obj = {
+    [s + "name"]:"sohan",
+    age : 22,
+    detail: function(){
+        return `${this.firstname} age is ${this.age}`
+    }
+};
+console.log(obj);
+console.log(obj.detail());
 
